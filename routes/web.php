a8811003cad3798fb76c5708791122baca816917
Wagner Sorio <wagnersorio@gmail.com>
2020-12-18 07:40:30 -0300

    adding plan functionality 01

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Auth::routes();
 
